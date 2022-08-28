f7ad8074c8b8ba8d51e50efd3e35a86bea43239e
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-08-28 19:47:26 +0300

    Update_task_02_js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,42 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientEl = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingredient;
+      ingredientItem.classList.add("item");
+  return ingredientItem;
+}); 
+document.querySelector("#ingredients").append(...ingredientEl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ingredientsEl = ingredients.map((ingredient) => {
+//   const ingredientItem = document.createElement("li");
+//   ingredientItem.classList.add("item");
+//   ingredientItem.textContent = ingredient;
+//   return ingredientItem;
+// });
+
+// document.querySelector("#ingredients").append(...ingredientsEl);
+
+
