7068a63118305ddc17696f53c952091aba07a3ed
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-08-30 13:19:29 +0300

    Update_task_08_js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('login-form');
+form.addEventListener('submit', setSubmit);
 
+function setSubmit(event) {
+    event.preventDefault();
+    const formElem = event.currentTarget.elements;
+    const mail = formElem.email.value;
+    const password = formElem.password.value;
+    if (email.value === '' || password.value === '') {
+      alert ('Все поля должны быть заполнены');
+    }
+
+    const formData = {
+        mail,
+        password ,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+
+}
