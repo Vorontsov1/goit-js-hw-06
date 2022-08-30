ce947f1b0862087ad9244a3645b9c3ffbd32be48
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-08-30 13:21:33 +0300

    07

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,5 @@ const textInput = document.querySelector('#text');
 const typeRange = document.querySelector('#font-size-control');
 
 typeRange.addEventListener('input', (event) => {
-    textInput.style.fontSize = event.currentTarget.value +'px';
+    textInput.style.fontSize = event.currentTarget.value + 'px';
 });
