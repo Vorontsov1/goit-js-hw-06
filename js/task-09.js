5c75227942408c9e15dfd4309685e8dc29450095
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-08-31 22:17:45 +0300

    Update_task_09_js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector('body');
+const btnElem = document.querySelector('.change-color');
+const spanText = document.querySelector('.color');
+btnElem.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const changeColor = getRandomHexColor();
+  bodyRef.style.background = changeColor;
+  spanText.textContent = changeColor;
+  console.log(changeColor);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
