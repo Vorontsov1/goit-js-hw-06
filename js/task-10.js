23c04804228a02e411c99ad665ddeb9c09e6f918
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-02 22:08:49 +0300

    Update_task_10_js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+const refs = {
+  input: document.querySelector('#controls input'),
+  createButtonElem: document.querySelector('[data-create]'),
+  destroyButtonElem: document.querySelector('[data-destroy]'),
+  wrapper: document.querySelector('#boxes')
+};
+const createBoxes = () => {
+  refs.wrapper.textContent = "";
+  const amount = Number(refs.input.value);
+  const items = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.background = getRandomHexColor();
+    div.style.width = i * 10 + 20 + "px";
+    div.style.height = i * 10 + 20 + "px";
+
+    items.push(div);
+  }
+  refs.wrapper.append(...items);
+};
+
+refs.createButtonElem.addEventListener('click', createBoxes);
+refs.destroyButtonElem.addEventListener('click', deleteBoxes);
+
+
+function deleteBoxes() {
+  refs.wrapper.textContent = '';
+  refs.input.value = '';
+  amount = 0;
+}
+
+boxes.style.displey = 'flex';
+boxes.style.listStyle = 'none';
+boxes.style.flexWrap = 'wrap';
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
